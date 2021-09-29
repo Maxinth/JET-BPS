2cfaa07c2c08730cfc4c1ba78653b4178818203b
folayarn <folayanshola@gmail.com>
2021-09-29 13:54:48 +0100

    login and forgot page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'
 import {BrowserRouter as Router} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import  store from './store/store'
